4f94c2c7a4beedf1e98555a22d19870cb28d209c
Leonel <lelobo75@gmail.com>
2022-07-10 22:02:17 +0200

    tercerleccion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
 import { Component } from 'react';
 
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      movieName: 'Indiana Jones',
+
+    }
+  }
   render(){
     return (
       <div className="App">
         <h1>Welcome te Lareveller</h1>
+        <p>My favorite movie is {this.state.movieName}.</p>
+        <button onClick={() =>{
+          this.setState({movieName: "Indiana Jones 2"})
+        }}
+        >
+         Cambiar el titulo 
+        </button>
+        <button onClick={() =>{
+          this.setState({movieName: "Indiana Jones 3"})
+        }}
+        >
+         Cambiar el titulo 
+        </button>
       </div>
     )
   } 
