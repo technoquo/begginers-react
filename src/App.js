218cf677a4191adcdceeb734a09b1e881d48667b
Leonel <lelobo75@gmail.com>
2022-07-11 18:40:20 +0200

    apimovie

--- src/App.js
+++ src/App.js
@@ -7,36 +7,30 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      movies: [
-        {id:1,name: 'Indiana Jones 1', year: 1981},
-        {id:2,name: 'Indiana Jones 2', year: 1984},
-        {id:3,name: 'Indiana Jones 3', year: 1989},
-        {id:4,name: 'Indiana Jones 4', year: 2008},
-        {id:5,name: 'Indiana Jones 5', year: 2023},
-      ]
+      movies: []
 
     }
   }
-  changeName =(event) => {
-    this.setState({
-      movies: [
-        {name: event.target.value, year: 1981},
-        {name: 'Batman 2', year: 1984},
-        {name: 'Batman 3', year: 1989},
-        {name: 'Batman 4', year: 2008},
-        {name: 'Batman 5', year: 2023},
-      ]
-    })
+
+   componentDidMount(){
+      fetch('https://fake-movie-database-api.herokuapp.com/api?s=batman'
+      
+      ).then(response => response.json())
+        .then((apiMovies) => 
+           this.setState(() => {
+             return { movies: apiMovies['Search'] }
+           }
+       ))
   }
   render(){
     return (
       <div className="App">
         <h1>Welcome te Lareveller</h1>
         {this.state.movies.map((movie)=>{
-              return <h2 key={movie.id}>My favorite movie is {movie.name}{" "}{movie.year}</h2>
+              return <h2 key={movie.imdbID}>My favorite movie is {movie.Title}{" "}{movie.Year}</h2>
         })}
      
-        <input type="text" onChange={this.changeName} value={this.state.movies[0].name}/>
+       
        
       </div>
     )
