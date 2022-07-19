27b72699b389280ce3c0ad3f0617448a2e5330e2
Leonel <lelobo75@gmail.com>
2022-07-19 11:54:32 +0200

    convertclass

--- src/Components/movie-card/movie-card.jsx
+++ src/Components/movie-card/movie-card.jsx
@@ -1,22 +1,19 @@
-import { Component } from "react";
-
-class MovieCard extends Component {
-    render() {
-
-        const { Poster, Title, Year } = this.props.movie
-        return <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-            <img className="rounded-t-lg" src={Poster} alt={Title} width="182" height="268"></img>
-            <div className="p-5">
-                <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        {Title}</h5>
+const MovieCard = ({movie}) => {
+    const { Poster, Title, Year } = movie
+ return(
+    <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+             <div className=" flex justify-center pt-3">
+             <img className="rounded-t-lg" src={Poster} alt={Title} width="182" height="268"></img>
+             </div>          
+             <div className="p-5">
+                 <a href="#">
+                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                         {Title}</h5>
                 </a>
-                <span className="mr-4">{Year}</span>
-            </div>
-        </div>
-    }
-
-
+                 <span className="mr-4">{Year}</span>
+             </div>
+         </div>
+   )
 }
 
 export default MovieCard
